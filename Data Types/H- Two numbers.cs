05f03a3462b0f49f0f53a433312a17519316dcb3
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:52:24 +0200

    Add files via upload

---
+++ Data Types/H- Two numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            long A = long.Parse(input[0]);
+            long B = long.Parse(input[1]);
+            Console.WriteLine($"floor {A} / {B} = {A / B}");
+            Console.WriteLine($"ceil {A} / {B} = {(A + B - 1) / B}");
+            Console.WriteLine($"round {A} / {B} = {(A + B / 2) / B}");
+        }
+    }
+}
