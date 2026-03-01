bceac5b64952d9848d814ee00028bbc722dc0845
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:53:26 +0200

    Add files via upload

---
+++ Data Types/I-Welcome for you with Conditions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            String[] input = Console.ReadLine().Split(' ');
+            long A = long.Parse(input[0]);
+            long B = long.Parse(input[1]);
+            if ((A > B) || (A == B))
+                Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+
+        }
+    }
+}
