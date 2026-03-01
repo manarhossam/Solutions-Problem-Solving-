a44c6a7a4bfc13de57824050d43141dac4237e41
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:47:23 +0200

    Add files via upload

---
+++ Data Types/D- Difference.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            String[] arr = Console.ReadLine().Split(' ');
+            long A = long.Parse(arr[0]);
+            long B = long.Parse(arr[1]);
+            Console.WriteLine($"{A} + {B} = {A + B}");
+            Console.WriteLine($"{A} * {B} = {A * B}");
+            Console.WriteLine($"{A} - {B} = {A - B}");
+
+        }
+    }
+}
