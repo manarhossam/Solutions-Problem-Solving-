4e4bdb729f54d4fa9c2f81c5674e563901a2ae30
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:54:11 +0200

    Add files via upload

---
+++ Data Types/J- Multiples.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            String[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            if (A % B == 0 || B % A == 0)
+                Console.WriteLine("Multiples");
+            else Console.WriteLine("No Multiples");
+
+
+        }
+    }
+}
