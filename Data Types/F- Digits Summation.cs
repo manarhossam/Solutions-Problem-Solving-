ca583624073c31e645a10897b59d5f3c0fddbd71
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:51:27 +0200

    Add files via upload

---
+++ Data Types/F- Digits Summation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            String[] input = Console.ReadLine().Split(' ');
+            long A = long.Parse(input[0]);
+            long B = long.Parse(input[1]);
+            long c = A % 10 + B % 10;
+            Console.WriteLine(c);
+        }
+    }
+}
