5074e33ba0557022327f36e4d99efc1130068230
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:50:29 +0200

    Add files via upload

---
+++ Data Types/E- Area of circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            double π = 3.141592653;
+            float radius = float.Parse(Console.ReadLine());
+            Console.WriteLine($"{π * (radius * radius)}");
+        }
+    }
+}
