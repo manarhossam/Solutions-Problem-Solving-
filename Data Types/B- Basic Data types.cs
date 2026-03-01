9685abb15b0698383ae06d1fd89eb2c974b10e71
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:31:35 +0200

    Add files via upload

---
+++ Data Types/B- Basic Data types.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            String[] str = Console.ReadLine().Split(' ');
+            int a = int.Parse(str[0]);
+            long b = long.Parse(str[1]);
+            char c = char.Parse(str[2]);
+            float f = float.Parse(str[3]);
+            double d = double.Parse(str[4]);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+
+           
+
+        }
+    }
+}
