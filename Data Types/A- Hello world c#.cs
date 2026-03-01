8bdd2e10ffeb731b6cd6bc21974f849a4c31fafa
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 00:33:14 +0200

    Add files via upload

---
+++ Data Types/A- Hello world c#.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            String name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}");
+
+
+        }
+    }
+}
