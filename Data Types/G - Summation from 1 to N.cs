1435f90a5f46db511148e66219058c64223863ab
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-02 22:03:01 +0200

    Add files via upload

---
+++ Data Types/G - Summation from 1 to N.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            //String[] input = Console.ReadLine().Split(' ');
+            //int A = int.Parse(input[0]);
+            //int B = int.Parse(input[1]);
+            //if (A % B == 0 || B % A == 0)
+            //    Console.WriteLine("Multiples");
+            //else Console.WriteLine("No Multiples");
+            long N =long.Parse(Console.ReadLine());   
+            long m =N*(N+1)/2;
+            Console.WriteLine(m);
+
+        }
+    }
+}
