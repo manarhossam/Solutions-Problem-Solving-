d053be9322939343e954b5acfb0606c8080a3782
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-05 22:09:18 +0200

    Add files via upload

---
+++ Data Types/N- Char.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            char m =char.Parse( Console.ReadLine());
+            if(m>='a'&&  m<= 'z')
+                Console.WriteLine((char) (m-32));
+            else Console.WriteLine((char)( m-32));
+
+        }
+    }
+}
