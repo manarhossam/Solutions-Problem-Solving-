d761bf636e764b9a6822f581e16b2bb23173d158
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-06 02:37:58 +0200

    Add files via upload

---
+++ Data Types/M - Capital or Small or Digit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace solutions_codeforces
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+           char X=char.Parse(Console.ReadLine());
+            if (X >= 'A' && X <= 'Z')
+            {
+                Console.WriteLine("ALPHA");
+                Console.WriteLine("IS CAPITAL");
+            }
+            else if (X >= 'a' && X <= 'z')
+            {
+                Console.WriteLine("ALPHA");
+                Console.WriteLine("IS SMALL");
+            }
+            else
+                Console.WriteLine("IS DIGIT");
+
+        }
+    }
+}
