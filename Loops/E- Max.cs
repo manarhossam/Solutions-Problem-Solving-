a12da1913cac6e50b0dfce2ff967c3fb39647886
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-07 23:10:58 +0200

    Add files via upload

---
+++ Loops/E- Max.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleAppode
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            String[] input = Console.ReadLine().Split(' ');
+            int max = 0;
+            for (int i = 0; i < number; i++)
+            {
+                int x = int.Parse(input[i]);
+
+                if (x > max)
+                {
+                    max = x;
+
+                }
+
+
+            }
+            Console.WriteLine(max);
+        
+
+    }
+    }
+}
