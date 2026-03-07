1084bb8c5c324087c87fff84e3a3a570a5efd966
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-07 23:07:43 +0200

    Add files via upload

---
+++ Loops/B- Even Numbers.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleAppode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < 2)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                for (int i = 2; i <= number; i += 2)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        
+    }
+    }
+}
