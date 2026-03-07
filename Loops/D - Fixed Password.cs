6e822953529707b60d2d7d7e2a5c3372ad418810
Manar Hossam <87241075+manarhossam@users.noreply.github.com>
2026-03-08 00:01:58 +0200

    Add files via upload

---
+++ Loops/D - Fixed Password.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleAppode
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                int password = int.Parse(Console.ReadLine());
+                if (password == 1999) { 
+                    Console.WriteLine("Correct");
+                break; }
+                else {
+                    Console.WriteLine("Wrong");
+                    }
+            }
+        }
+    }
+}
